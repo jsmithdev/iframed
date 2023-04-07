bc48c977c7b85678ae0d0b32f5990c52a816dda1
Jamie Smith <me@jsmith.dev>
2023-04-07 10:21:34 -0400

    enhance: dynamic url and params

--- README.md
+++ README.md
@@ -8,12 +8,15 @@ An LWC component to embed a URL in an iframe in Salesforce via Builder
 
 1. Add the iFramed component to a page via Builder
 2. Set the URL to the URL you want to embed
-3. Set the Height to the height of the iframe using css acceptable values (500px, 100%, 50vh, etc)
-4. Set the Width to the width of the iframe using css acceptable values (500px, 100%, 50vw, etc)
+3. Optionally, set the parameters to the params you want to add
+4. Set the Height to the height of the iframe using css acceptable values (500px, 100%, 50vh, etc)
+5. Set the Width to the width of the iframe using css acceptable values (500px, 100%, 50vw, etc)
+
+> You use template syntax to pass in values from the current record e.g. `https://www.google.com/search?q={Account.Name}` or `https://example.com/{Contact.ExternalId__c}`
+
+![iFramed in Builder](/docs/iframed.png)
 
 > Be sure the URL you are embedding is accessible from the Salesforce domain by adding the domain to the CSP Trusted Sites setting in Setup
-
-![iFramed in Builder](https://i.imgur.com/Q0jRFRQ.png)
 
 ## Deployment
 
@@ -23,7 +26,36 @@ An LWC component to embed a URL in an iframe in Salesforce via Builder
 
 ### Deploy via SFDX
 
-`sfdx force:source:deploy -p force-app`
+Covert with SFDX; This creates a folder called `deploy`
+
+```bash
+sfdx force:source:convert -r force-app -d deploy
+```
+
+Now you can deploy from the resulting `deploy` directory
+
+```bash
+sfdx force:mdapi:deploy -d deploy -w -1 --verbose
+```
+
+📌 Above deploys to the default org set
+
+- Add -u user@domain.com or -u alias to deploy else where
+- To run tests add -l RunSpecifiedTests -r ApexTestName
+
+Results should more or less mirror below
+
+```bash
+Using specified username me(a)jsmith.dev
+
+Job ID | 0Af1U000015XR14SAG
+MDAPI PROGRESS | ████████████████████████████████████████ | 10/10 Components
+
+TYPE FILE NAME ID
+──────────────────────── ──────────────────────────────────── ────────────────────────── ──────────────────
+deploy/package.xml package.xml
+ExampleClass deploy/classes/Example.cls Example 01p1U00000QWibCQAT
+```
 
 ---
 
--- force-app/main/default/lwc/iframed/iframed.js
+++ force-app/main/default/lwc/iframed/iframed.js
@@ -1,11 +1,245 @@
-import { api,LightningElement } from 'lwc';
+import { api, LightningElement } from 'lwc';//wire,
 
-export default class Iframed extends LightningElement {
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+import userId from '@salesforce/user/Id';
+//import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+//import User_Name from '@salesforce/schema/User.Name';
+
+import findParams from '@salesforce/apex/Iframed.findParams';
+
+
+export default class IFramed extends LightningElement {
 	@api url;
 	@api height;
 	@api width;
+	@api params;
+	@api recordId;
+	@api debug = false;
+
+	isLoading = true;
+	iframeUrl = '';
+	iframeParams = '';
+
+	get iframeSrc() {
+		return this.iframeUrl + this.iframeParams;
+	}
 
 	get iframeStyle() {
 		return `height:${this.height};width:${this.width};`;
+	}
+
+    //get username() {
+    //    return getFieldValue(this.user?.data, User_Name);
+    //}
+
+    //@wire(getRecord, { recordId: '$userId', fields: [ User_Name, ] })
+    //user;
+
+	connectedCallback() {
+		this.init();
+	}
+	renderedCallback() {
+		this.applyDimensions();
+	}
+
+	debugOut() {
+
+		console.log( JSON.parse(JSON.stringify({
+			params: this.params,
+			username: this.username,
+			iframeSrc: this.iframeSrc,
+			iframeParams: this.iframeParams,
+			_params_normal: this._params_normal,
+			_params: this._params,
+			_processed: this._processed,
+			_processedParams: this._processedParams,
+		})));
+	}
+
+	
+	async init() {
+		try {
+			
+			if(!this.url){
+				const msg = 'Please provide a URL for the iframe';
+				this.showToast(msg, msg, 'error');
+			}
+			
+			if(this.url){
+				this.iframeUrl = this.url;
+			}
+			else {
+				this.iframeUrl = 'https://example.com';
+			}
+			// handle dynamic url
+			if(this.iframeUrl){
+				if(this.iframeUrl.includes('{')){
+					// we have dynamic url
+					console.log('iframe dyn iframeUrl: ' + this.iframeUrl);
+					const url = await this.processURL(this.iframeUrl);
+					this.iframeUrl = url;
+				}
+			}
+			// handle dynamic params
+			if(this.params){
+				// set params
+				if(this.params.includes('{')){
+					// we have dynamic param/s
+					//console.log('iframe dyn param: ' + this.url);	
+					const params = await this.processParams(this.params);
+					this.iframeParams = params;
+				}
+				else {
+					this.iframeParams = this.params;
+					//console.log('iframe not dyn param: ' + this.iframeUrl);	
+				}
+			}
+	
+			console.log('iframe url: ' + JSON.parse(JSON.stringify({
+				iframeUrl: this.iframeUrl,
+				iframeParams: this.iframeParams,
+			})));
+			
+			this.isLoading = false;
+		}
+		catch (error) {
+			this.handleError(error);
+		}
+	}
+
+	applyDimensions() {
+		if (this.height) {
+			this.template.querySelector('.container').style.height = this.height;
+		}
+		if (this.width) {
+			this.template.querySelector('.container').style.width = this.width;
+		}
+	}
+
+	async processParams(input){
+
+		const qIndex = input.indexOf('?')
+
+		// if no dynamic params, return the normal params
+		if(qIndex === -1) return ``;
+
+		const array = input.slice(qIndex + 1)
+
+		const params = []
+		const params_normal = []
+
+		// get the param name, object and field
+		array.split('&').forEach(p => {
+
+			console.log('dyn param p: ' + p);
+
+			if(!p.includes('=')) return;
+			if(!p.includes('{') || !p.includes('}')) {
+				params_normal.push({
+					param: p.split('=')[0],
+					value: p.split('=')[1],
+				})
+				return;
+			}
+
+			const [param, value] = p.split('=')
+			const [sobjectApi, field] = value
+				.replace('{', '')
+				.replace('}', '')
+				.split('.');
+
+			const res = {
+				param,
+				sobjectApi,
+				field,
+				value: '',
+			}
+
+			if(res.sobjectApi.toLowerCase() === 'user'){
+				res.recordId = userId;
+			}
+			else {
+				res.recordId = this.recordId || userId;
+			}
+
+			params.push(res)
+		})
+
+		this._params_normal = params_normal;
+		this._params = params;
+
+		// if no dynamic params, return the normal params
+		if(!params.length) return `?${this.buildParams(...params_normal)}`
+
+		// call apex to get the values
+		const processed = await findParams({
+			params: params
+		})
+
+		this._processed = processed;
+		
+		// build the param string
+		const result = `?${this.buildParams(...params_normal, ...JSON.parse(processed))}`
+
+		this._processedParams = result;
+
+		return result;
+	}
+
+	buildParams(...arr){
+		return arr.map(p => `${p.param}=${p.value}`).join('&')
+	}
+
+	async processURL(input){
+
+		const regex = /{([^}]+)}/g;
+
+		const matches = input.match(regex);
+
+		const params = matches.map(match => {
+			const s = match.replace('{', '').replace('}', '');
+			const [sobjectApi, field] = s.split('.')
+			
+			const res = {
+				field,
+				sobjectApi,
+				param: match,
+				value: '',
+			}
+
+			if(res.sobjectApi.toLowerCase() === 'user'){
+				res.recordId = userId;
+			}
+			else {
+				res.recordId = this.recordId || userId;
+			}
+
+			return res;
+		})
+
+		const res = await findParams({
+			params: params
+		})
+
+		return JSON.parse(res).reduce((acc, r) => {
+			const { param, value } = r;
+			return acc.replace(param, value);
+		}, input);
+	}
+
+	showToast(title, message, variant) {
+		const event = new ShowToastEvent({
+			title: title,
+			message: message,
+			variant: variant,
+		});
+		this.dispatchEvent(event);
+	}
+
+	handleError(error) {
+		console.log('iframe error: ' + error)
+		const msg = error?.body?.message || error?.message || error || 'Unknown iframe error';
+		this.showToast(msg, msg, 'error');
 	}
 }
